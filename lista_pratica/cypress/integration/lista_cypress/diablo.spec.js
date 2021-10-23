435b7b43aa362f3e78b05721a1839268f5504802
Gabriel Pivoto <gabriel-pivoto@outlook.com>
2021-10-23 14:13:26 -0300

    Testes atualizados

--- lista_pratica/cypress/integration/lista_cypress/diablo.spec.js
+++ lista_pratica/cypress/integration/lista_cypress/diablo.spec.js
@@ -30,12 +30,37 @@ describe('Cenário de teste: Testar as funcionalidades do site Diablo III', () =
         cy.get('span[aria-disabled="false"]').should('contain.text', 'R$199,00');        
     })
 
-    it('Caso de teste: Testar o preço do jogo no Xbox', () => {
-        cy.visit('https://us.diablo3.com/pt-br/calculator/wizard');
 
-        cy.get('#skills > :nth-child(2) > :nth-child(1)').click();
-        //cy.get(':nth-child(2) > :nth-child(1) > .skill-button > .skill-slot').trigger('mouseover').click();
+    it.skip('Caso de teste: Testar o preço do jogo no PS4', () => {
+        cy.visit('https://us.diablo3.com/pt-br/');
+        cy.get('.menu-purchase > a > span').click();
+        cy.get('.Button--ps4 > .Button-link').click();
+        cy.get('.psw-t-title-m').should('contain.text', 'R$ 249,00');        
+    })
+
+
+    it.skip('Caso de teste: Testar a calculadora de habilidades', () => {
+
+        cy.visit('https://us.diablo3.com/pt-br/calculator/follower');
+        cy.get('#templar > .tiers > .tier-0 > .button-1 > .d3-icon > .frame').click();
+        cy.get('#templar > .tiers > .tier-1 > .button-2 > .d3-icon > .frame').click();
+        cy.get('#templar > .tiers > .tier-2 > .button-5 > .d3-icon > .frame').click();
+        cy.get('#templar > .tiers > .tier-3 > .button-7 > .d3-icon > .frame').click();
+        cy.get('#templar > .meta-info > .tip > .required-level').should('contain.text',20);
          
     })
 
+    it('Caso de teste: Rodar o vídeo da história', () => {
+        cy.visit('https://us.diablo3.com/pt-br/');
+        cy.get('.menu-game > a > span').click();
+        cy.get('.game-index-callout').click();
+        cy.get('#day').select(31);
+        cy.get('#month').select('Agosto');
+        cy.get('#year').select('1999');
+        cy.get('.button-right').click();
+        cy.get('#video-play').click();
+              
+    })
+
 })
+
